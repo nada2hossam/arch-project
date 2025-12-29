bd04e6f3180670369f0b8383260bcad9e8f8d0ac
Manar-Maher <M.Maher2381@nu.edu.eg>
2025-12-29 05:21:08 +0200

    Update Instruction_Memory module initialization
    Refactor instruction memory initialization and add new instructions.

--- sources_1/instruction_memory.v
+++ sources_1/instruction_memory.v
@@ -1,32 +1,18 @@
-`timescale 1ns / 1ps
-
 module Instruction_Memory (
     input  [63:0] address,
     output [31:0] instruction
 );
 
-    reg [31:0] memory [0:255];
+  reg [31:0] memory[0:255];
 
-    assign instruction = memory[address[9:2]];
+  assign instruction = memory[address[9:2]];
 
-    integer i;
-    initial begin
-        for (i = 0; i < 256; i = i + 1) begin
-            memory[i] = 32'd0;
-        end
-        
-        memory[0] = 32'h00A00093;
-
-        memory[1] = 32'h01400113;
-
-        memory[2] = 32'h002081B3;
-
-        memory[3] = 32'h00302023;
-
-        memory[4] = 32'h00002203;
-
-        memory[5] = 32'hFE418E63;
-        
-    end
+  integer i;
+  initial begin
+    for (i=0; i<256; i=i+1) memory[i] = 32'd0; 
+    memory[0] = 32'h00000033; 
+    memory[1] = 32'h00A50533; 
+    memory[2] = 32'h40058533;
+  end
 
 endmodule
