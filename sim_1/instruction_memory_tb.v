53cfe5f7a17c13f6ab5f0c1f3693a6b6882098f2
Manar-Maher <M.Maher2381@nu.edu.eg>
2025-12-29 05:24:27 +0200

    Refactor instruction memory testbench

--- sim_1/instruction_memory_tb.v
+++ sim_1/instruction_memory_tb.v
@@ -1,40 +1,18 @@
-`timescale 1ns / 1ps
+module Instruction_Memory (
+    input  [63:0] address,
+    output [31:0] instruction
+);
 
-module instruction_memory_tb;
+  reg [31:0] memory[0:255];
 
-    reg  [63:0] pc_addr;
-    wire [31:0] instr_out;
-    reg  [31:0] expected;
+  assign instruction = memory[address[9:2]];
 
-    Instruction_Memory UUT (
-        .address(pc_addr),
-        .instruction(instr_out)
-    );
-
-    initial begin
-        $display("Time\tAddress\t\tInstruction (Hex)\tExpected");
-
-        pc_addr = 64'd0;
-        expected = 32'h0000_0033; 
-        #10;
-        $display("%0t\t%0d\t\t%h\t\t%h", $time, pc_addr, instr_out, expected);
-
-        pc_addr = 64'd4;
-        expected = 32'h00A5_0533; 
-        #10;
-        $display("%0t\t%0d\t\t%h\t\t%h", $time, pc_addr, instr_out, expected);
-
-        pc_addr = 64'd8;
-        expected = 32'h4005_8533; 
-        #10;
-        $display("%0t\t%0d\t\t%h\t\t%h", $time, pc_addr, instr_out, expected);
-
-        pc_addr = 64'd12;
-        expected = 32'h0000_0000;
-        #10;
-        $display("%0t\t%0d\t\t%h\t\t%h", $time, pc_addr, instr_out, expected);
-
-        $finish;
-    end
+  integer i;
+  initial begin
+    for (i=0; i<256; i=i+1) memory[i] = 32'd0; 
+    memory[0] = 32'h00000033; 
+    memory[1] = 32'h00A50533; 
+    memory[2] = 32'h40058533;
+  end
 
 endmodule
